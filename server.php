51d9d243463e4d9a64f3c87e88566e4a2a8fc3d2
Manny Munoz <1mannymunoz@gmail.com>
2019-04-02 15:30:43 -0700

    Finished outline for server.php

--- server.php
+++ server.php
@@ -4,17 +4,38 @@
 
   if(isset($_POST["mode"]) && is_valid_mode($_POST["mode"])) {
     $mode = $_POST["mode"];
-    if($mode === "add" && isset($_POST["assignment"]) && isset($_POST["duedate"]) &&
-      isset($_POST["class"])) {
-      add_assignment($_POST["class"], $_POST["assignment"], $_POST["duedate"]);
-    } else if($mode === "remove" && $_POST["assignment"]) {
-      remove_assignment();
-    } else if() {
-      update_assignment();
-    } else if() {
-      add_class();
-    } else if() {
-      remove_all();
+    if($mode === "add" || $mode === "removeAll" || $mode === "autoClass") {
+      if(isset($_POST["class"])) {
+        $class = $_POST["class"];
+        if($mode === "add") {
+          if(isset($_POST["assignment"]) && isset($_POST["duedate"])) {
+            add_assignment();
+          } else {
+            die("ERROR: NEED ASSIGNMENT OR DUEDATE");
+          }
+        } else if($mode === "removeAll") {
+          remove_all();
+        } else if($mode === "autoClass") {
+          auto_add();
+        }
+      } else {
+        die("ERROR: NEED CLASS");
+      }
+    } else if($mode === "remove" || $mode === "update") {
+      if(isset($_POST["assignment"])) {
+        $assignment = $_POST["assignment"];
+        if($mode === "update") {
+          if(isset($_POST["timestamp"]) && isset($_POST["status"])) {
+            update_assignment();
+          } else {
+            die("ERROR: NEED TIMESTAMP AND STATUS");
+          }
+        } else if($mode === "remove") {
+          remove_assignment();
+        }
+      } else {
+        die("ERROR: NEED ASSIGNMENT");
+      }
     }
   }
 
@@ -23,7 +44,28 @@
     return in_array($mode, $valid_modes);
   }
 
-  function add_assignment($netid, $class, $assignment, $duedate) {
-    echo "Class: " . $class . " Assignment: " . $assignment . " Due date: " . $duedate;
+  function add_assignment() {
+    echo "Successfully added";
   }
+
+  function remove_assignment() {
+    echo "Successfully removed";
+  }
+
+  function update_assignment() {
+    echo "Successfully updated";
+  }
+
+  function auto_add() {
+    add_assignment();
+  }
+
+  function remove_all() {
+    remove_assignment();
+  }
+
+  // function add_assignment($netid, $class, $assignment, $duedate) {
+  //   echo "Class: " . $class . " Assignment: " . $assignment . " Due date: " . $duedate;
+  // }
+
 ?>
