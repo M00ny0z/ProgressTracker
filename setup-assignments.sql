279cc778f0ea675213d52e419d1ca25ed1f7ebbe
Manny Munoz <1mannymunoz@gmail.com>
2019-04-02 16:13:12 -0700

    Created setup-file for assignments database

---
+++ setup-assignments.sql
@@ -0,0 +1,16 @@
+-- Setup file for Assignments database
+USE assignments;
+
+DROP TABLE IF EXISTS Assignments;
+CREATE TABLE Assignments(
+  netid        VARCHAR(20) PRIMARY KEY NOT NULL,
+  class        VARCHAR(10) NOT NULL,
+  name         VARCHAR(20) NOT NULL,
+  completion   TINYINT(1) NOT NULL,
+  grade        TINYINT(101),
+  duedate      TIMESTAMP,
+  turnin       TIMESTAMP
+);
+
+INSERT INTO Assignments(netid, class, name, completion, grade, duedate, turnin) VALUES
+  ('em66', 'CSE154', 'Set!', TRUE, 100, '', '2019-04-02 04:03:07');
