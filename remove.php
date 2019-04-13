5a6de393cdfa0365279d6e927b4abf7f419f1439
Manny Munoz <1mannymunoz@gmail.com>
2019-04-12 23:21:39 -0700

    Commented functions and refactored decision pathway

--- remove.php
+++ remove.php
@@ -4,26 +4,37 @@
 
   if(isset($_POST["mode"])) {
     $mode = $_POST["mode"];
-    if($mode === "removeAll") {
-      if(isset($_POST["class"])){
-        remove_all($_POST["class"]);
+    if(isset($_POST["class"])) {
+      $class = $_POST["class"];
+      if($mode === "removeAll") {
+        remove_all($class);
+      } else if(isset($_POST["assignment"])) {
+        remove_assignment($_POST["assignment"]);
       } else {
-        handle_error("NEED A CLASS TO REMOVE FROM");
+        handle_error("NEED AN ASSIGNMENT TO REMOVE");
       }
-    } else if(isset($_POST["assignment"])) {
-      remove_assignment($_POST["assignment"]);
     } else {
-      handle_error("NEED AN ASSIGNMENT TO REMOVE");
+      handle_error("NEED A CLASS TO REMOVE FROM");
     }
   } else {
     handle_error("NEED A VALID MODE");
   }
 
+  /**
+   * Removes all of the assignments of a specified class, essentially removes the class from the
+   * user
+   * @param $class {string} - The class to remove from the user
+  */
   function remove_all($class) {
     remove_assignment($class);
   }
 
-  function remove_assignment($assignment) {
+  /**
+   * Removes a specified assignment of a specified class
+   * user
+   * @param $class {string} - The class to remove from the user
+  */
+  function remove_assignment($assignment, $class) {
     echo "Successfully removed";
   }
 ?>
