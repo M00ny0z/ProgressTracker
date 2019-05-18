f8b9fd01933c45b4497c76806d93c4c36202dc2c
Manny Munoz <1mannymunoz@gmail.com>
2019-05-18 16:31:27 -0700

    Fixed assignment name length, added enrollments table to track student classes

--- setup-students.sql
+++ setup-students.sql
@@ -1,4 +1,4 @@
--- Setup file for Assignments database
+-- Setup file for Students database
 USE students;
 
 DROP TABLE IF EXISTS Notes;
@@ -10,9 +10,9 @@ CREATE TABLE Notes(
 
 DROP TABLE IF EXISTS Assignments;
 CREATE TABLE Assignments(
-  netid        VARCHAR(20) PRIMARY KEY NOT NULL,
+  netid        VARCHAR(20) NOT NULL,
   class        VARCHAR(10) NOT NULL,
-  name         VARCHAR(20) NOT NULL,
+  name         VARCHAR(40) NOT NULL,
   completion   TINYINT(1) NOT NULL,
   grade        TINYINT(101),
   duedate      TIMESTAMP,
@@ -25,5 +25,24 @@ CREATE TABLE Students(
   name         VARCHAR(30) NOT NULL
 );
 
+DROP TABLE IF EXISTS Enrolls;
+CREATE TABLE Enrolls(
+  class        VARCHAR(20) NOT NULL,
+  netid        VARCHAR(30) NOT NULL
+);
+
+
+INSERT INTO Students(netid, name) VALUES
+  ('em66', "Emmanuel Munoz");
+
 INSERT INTO Notes(netid, notes, creation) VALUES
   ('em66', 'Manny fucked up', '2019-04-02 04:03:07');
+
+INSERT INTO Enrolls(class, netid) VALUES
+  ('CSE414', 'em66');
+
+INSERT INTO Enrolls(class, netid) VALUES
+  ('MATH126', 'em66');
+
+INSERT INTO Enrolls(class, netid) VALUES
+  ('MATH197', 'em66');
